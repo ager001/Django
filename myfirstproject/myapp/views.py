3a53740d73a3b17c395a360ac242ed6c932ce11e
ager001 <152964009+ager001@users.noreply.github.com>
2026-05-07 13:45:10 +0300

    Add template rendering to myapp view and create myfirsthtml.html

--- myfirstproject/myapp/views.py
+++ myfirstproject/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def myapp(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirsthtml.html')
+    return HttpResponse(template.render())
