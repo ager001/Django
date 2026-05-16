ed06e8449978d050017f61ca95bbd057304ce49a
ager001 <152964009+ager001@users.noreply.github.com>
2026-05-16 13:06:46 +0300

    Implement user listing in myapp view and create users.html template

--- myfirstproject/myapp/views.py
+++ myfirstproject/myapp/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Users
+
 def myapp(request):
-    template = loader.get_template('myfirsthtml.html')
-    return HttpResponse(template.render())
+    users = Users.objects.all().values()
+    template = loader.get_template('users.html')
+    context = {
+        'users': users,
+    }
+    return HttpResponse(template.render(context, request))
